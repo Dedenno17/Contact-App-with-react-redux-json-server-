00fa2e7f02ee4d4fbea5d5d35c914fd9382f39f3
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-03-17 16:03:22 +0700

    membuat route untuk home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
