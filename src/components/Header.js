a3353666aa50699fa8451f6cedb1320b7fa83188
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-03-17 17:54:54 +0700

    membuat route home ketika logo di klik, memeberi style ketika button add di hover

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <header className="header">
             <div className="logo">
-                <p>React Redux Contact App</p>
+                <Link to='/'>
+                    <p>React Redux Contact App</p>
+                </Link>
             </div>
         </header>
     );
