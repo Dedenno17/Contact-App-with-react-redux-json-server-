ae4e860ea5ea0999911c055185fa041a8b54014b
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-03-18 22:29:21 +0700

    memberi style baru pada edit & delete button

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,18 +1,42 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const Edit = () => {
+
+    const { id } = useParams();
+    const [contact, setContact] = useState('');
+    // const [ updateName, setUpdateName ] = useState('');
+    // const [ updateEmail, setUpdateEmail ] = useState('');
+    // const [ updatePhone, setUpdatePhone ] = useState('');
+
+
+    useEffect(() => {
+
+        fetch('http://localhost:8000/contacts/' + id)
+            .then((res) => res.json())
+            .then((res) => {
+                setContact(res);
+            })
+
+    }, []);
+
+
     return (
         <div className="edit">
             <Link to='/'>Go Back</Link>
             <form>
                 <input
                     type='text'
+                    value={ contact.name }
                 />
                 <input
                     type='text'
+                    value={contact.email}
+
                 />
                 <input
                     type='text'
+                    value={contact.phone}
                 />
                 <div className="btn">
                     <button className="update">Update Contact</button>
